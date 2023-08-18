b19610ff8804cd9f1bc8a956e927397068442f3e
Vijay Vithal Joshi <123143472+Vj-r12@users.noreply.github.com>
2023-08-19 01:46:40 +0530

    Update Stored_Procedure_implementation_project_DDL.sql

--- Code File/Stored_Procedure_implementation_project_DDL.sql
+++ Code File/Stored_Procedure_implementation_project_DDL.sql
@@ -4,23 +4,23 @@
 -- Creating Sales_info Table.
 
 CREATE TABLE SALES_INFO (CUSTOMER_ID int primary key,
-						 PRODUCT_ID varchar,
+			 PRODUCT_ID varchar,
                          QUANTITY int,AMOUNT_PER_PRODUCT int, 
-						 TOTAL_AMOUNT int
-						);
+			 TOTAL_AMOUNT int
+			);
  
 
 -- Creating Revenue_info Table.						 
 
 CREATE TABLE REVENUE_INFO (PRODUCT_ID varchar PRIMARY KEY,
                            PRODUCT_NAME varchar,PRODUCT_REVENUE int
-						  );
+			  );
 
 
 -- Creating Inventory_info Table.
 
 CREATE TABLE INVENTORY_INFO (PRODUCT_ID varchar PRIMARY KEY,
                              PRODUCT_NAME varchar,OPENING_STOCK int,
-							 CLOSING_STOCK int, TOTAL_ITEM_SOLD_OF_PRODUCT int,
-							 STOCK_STATUS varchar
-							);
+			     CLOSING_STOCK int,TOTAL_ITEM_SOLD_OF_PRODUCT int,
+			     STOCK_STATUS varchar
+			    );
